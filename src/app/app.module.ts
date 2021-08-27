a1c9d552ed3e17768a31842c86e255882fbed368
SitrakaRandriamanana <mamitina.sitraka.randriamanana@gmail.com>
2021-08-27 13:13:03 +0300

    last changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SigninComponent
   ],
   imports: [
     BrowserModule,
