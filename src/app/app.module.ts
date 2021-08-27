498b2e3739e3dba9bfcb0d2d37885860fd1201e1
SitrakaRandriamanana <mamitina.sitraka.randriamanana@gmail.com>
2021-08-27 13:23:18 +0300

    signup updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { JoinComponent } from './signup/join.component';
 import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    JoinComponent,
     SignupComponent
   ],
   imports: [
